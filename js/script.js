7c818f4ac3bc80bbfabd70d67236ca4751aece04
eheslouis <eheslouis@gmail.com>
2016-05-19 15:42:10 +0700

    list added
    list added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+var initLocation = {lat: 21.063467, lng: 105.829238};
 var myPlaces = 
 [
 	{
@@ -11,33 +12,41 @@ var myPlaces =
 	{
 		position: {lat: 21.035302, lng: 105.849257},
 		title: "Old Quarter"
+	},
+	{
+		position: {lat: 21.036713, lng: 105.834731},
+		title: "Ho Chi Minh Mausoleum"
+	},
+	{
+		position: {lat: 21.047953, lng: 105.836979},
+		title: "Pagoda Trấn Quốc"
 	}
 
 ];
 
-var Marker = function (data){
+var Marker = function (data, map){
 	this.position = ko.observable(data.position);
 	this.title = ko.observable(data.title);
+	this.marker = new google.maps.Marker({
+		position: this.position(),
+		map: map,
+		title: this.title()
+	});
+	this.marker.setMap(map);
 };
 
-
 var ViewModel = function(){
-	console.log("viewModel");
 	var self = this;
 
-	this.myLatLng = {lat: 21.063467, lng: 105.829238};
 	this.map = new google.maps.Map(document.getElementById('map'), {
-		center: this.myLatLng,
-		zoom: 12
+		center: initLocation,
+		zoom: 13
   	});
-}; 
+	this.markersList = ko.observableArray([]);
+	myPlaces.forEach(function(placeItem){
+		self.markersList.push(new Marker(placeItem, self.map));
+	});
 
-// 	var marker = new google.maps.Marker({
-// 		position: myLatLng,
-// 		map: map,
-// 		title: 'my house'
-// 	});
-// };
-
+};
 
 ko.applyBindings(new ViewModel());
